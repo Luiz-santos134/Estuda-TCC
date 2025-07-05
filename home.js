89b18f4cb67cd8b4b531a2061e99d9beb0ccf30d
luiz claudio <luizclaudiosantos134@gmail.com>
2025-07-05 03:29:00 -0300

    fiz o style do simulado ficar no html, fiz a msg para a caixa do simulado para caso nao tenha simulados feitos, adicionei o resumo do home e poli o style

--- home.js
+++ home.js
@@ -1,8 +1,18 @@
 const currentPage = window.location.pathname.split("/").pop();
     const links = document.querySelectorAll(".menu-link");
-
     links.forEach(link => {
         if (link.getAttribute("href") === currentPage) {
             link.classList.add("active");
         }
     });
+
+let areaSimuladosFeitos = document.querySelector('.feitos p')
+
+if (areaSimuladosFeitos.textContent.trim() === ""){
+    areaSimuladosFeitos.innerHTML = `
+        <i class="fas fa-file-alt"></i>
+        Nenhum simulado realizado ainda.
+        <br>
+        Que tal fazer o primeiro?
+         `
+}
